7df0acaae5c7ed6205ab28172527be57b5ac223b
Daniel Quispe J <dgquispe@pucp.edu.pe>
2022-03-28 18:48:08 -0500

    ok2

--- SRC/reporte2.py
+++ SRC/reporte2.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
+
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 st.title('Reporte de emisiones Co2 en vehículos de Canadá')
