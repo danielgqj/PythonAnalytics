207d7ab389a270bfe9b6f648d6f9079c7039d9ad
Daniel Quispe J <dgquispe@pucp.edu.pe>
2022-03-28 17:46:10 -0500

    nuevo

---
+++ SRC/reporte2.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
+
+
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.title('Reporte de emisiones Co2 en vehículos de Canadá')
+
+
+
+data = pd.read_csv('./Co2/CO2 Emissions_Canada.csv')
+
+
+
+st.sidebar.header("Please Filter Here:")
+Marca = st.sidebar.multiselect(
+    "Select the City:",
+    options=data["Make"].unique(),
+    default=data["Make"].unique()
+)
+
+data_selection = data.query(
+    "Make == @Marca"
+)
+st.dataframe(data_selection)
+
+
+
+
