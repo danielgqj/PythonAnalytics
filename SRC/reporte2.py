dc1cd1565184cc8f76a3ea11f90c88a47269b08b
Daniel Quispe J <dgquispe@pucp.edu.pe>
2022-03-28 18:17:40 -0500

    ok

--- SRC/reporte2.py
+++ SRC/reporte2.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from pathlib import Path
-
+import plotly.express as px
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 st.title('Reporte de emisiones Co2 en vehículos de Canadá')
@@ -26,7 +25,6 @@ data_selection = data.query(
     "Make == @Marca"
 )
 st.dataframe(data_selection)
-
 
 
 
