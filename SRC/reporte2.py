1f2ab6dabd991a8dbb90ce22c44874b595f9d4b7
Daniel Quispe J <dgquispe@pucp.edu.pe>
2022-03-28 22:51:40 -0500

    strem

--- SRC/reporte2.py
+++ SRC/reporte2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import seaborn as sns
+
 
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
